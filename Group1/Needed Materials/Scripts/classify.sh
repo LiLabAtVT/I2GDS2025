046221acd9a1d20f90fdf3df840808b6e03c7ea2
peterfs-vt <peterfs@vt.edu>
2025-10-29 15:37:10 -0400

    Update classify.sh

--- Group1/Needed Materials/Scripts/classify.sh
+++ Group1/Needed Materials/Scripts/classify.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=normal_q
 #SBATCH --time=0-20:00:00
 #SBATCH --mem=20G
-#SBATCH --mail-user=peterfs@vt.edu
+#SBATCH --mail-user=yourusername@vt.edu
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
@@ -14,39 +14,39 @@ conda activate qiime2-amplicon-2025.7
 
 # Step 1: Cluster features de novo
 qiime vsearch cluster-features-de-novo \
---i-sequences /home/peterfs/practice/QIIME/sequences.qza \
---i-table /home/peterfs/practice/QIIME/table.qza \
+--i-sequences /path/to/your/directory/sequences.qza \
+--i-table /path/to/your/directory/table.qza \
 --p-perc-identity 0.97 \
---o-clustered-table /home/peterfs/practice/QIIME/table_97.qza \
---o-clustered-sequences /home/peterfs/practice/QIIME/rep_seqs_97.qza
+--o-clustered-table /path/to/your/directory/table_97.qza \
+--o-clustered-sequences /path/to/your/directory/rep_seqs_97.qza
 
 # Step 2: Classify sequences
 qiime feature-classifier classify-sklearn \
---i-reads /home/peterfs/practice/QIIME/rep_seqs_97.qza \
---i-classifier /home/peterfs/practice/files/silva-138-99-nb-classifier.qza \
---o-classification /home/peterfs/practice/QIIME/updated_taxonomy.qza \
+--i-reads /path/to/your/directory/rep_seqs_97.qza \
+--i-classifier /projects/intro2gds/I2GDS2025/TestData_LinuxPeerEval/G1_testdata/silva-138-99-nb-classifier.qza \
+--o-classification /path/to/your/directory/updated_taxonomy.qza \
 
 # Step 3: Tabulate metadata (Optional)
 qiime metadata tabulate \
---m-input-file /home/peterfs/practice/QIIME/updated_taxonomy.qza \
---o-visualization /home/peterfs/practice/QIIME/taxa-meta.qzv
+--m-input-file /path/to/your/directory/updated_taxonomy.qza \
+--o-visualization /path/to/your/directory/taxa-meta.qzv
 
 # Step 4: Filter table by taxonomy
 qiime taxa filter-table \
---i-table /home/peterfs/practice/QIIME/table_97.qza \
---i-taxonomy /home/peterfs/practice/QIIME/updated_taxonomy.qza \
+--i-table /path/to/your/directory/table_97.qza \
+--i-taxonomy /path/to/your/directory/updated_taxonomy.qza \
 --p-exclude mitochondria,chloroplast \
---o-filtered-table /home/peterfs/practice/QIIME/tax-class-filter-table.qza
+--o-filtered-table /path/to/your/directory/tax-class-filter-table.qza
 
-# OPTIONAL: Remove singletons
+# Step 5: Remove singletons
 qiime feature-table filter-features \
---i-table /home/peterfs/practice/QIIME/tax-class-filter-table.qza \
+--i-table /path/to/your/directory/tax-class-filter-table.qza \
 --p-min-frequency 2 \
---o-filtered-table /home/peterfs/practice/QIIME/feature-frequency-filtered-table.qza
+--o-filtered-table /path/to/your/directory/feature-frequency-filtered-table.qza
 
-# Step 5: Generate bar plots
+# Step 6: Generate bar plots
 qiime taxa barplot \
---i-table /home/peterfs/practice/QIIME/feature-frequency-filtered-table.qza \
---i-taxonomy /home/peterfs/practice/QIIME/updated_taxonomy.qza \
---m-metadata-file /home/peterfs/practice/files/bacteria_manifest.tsv \
---o-visualization /home/peterfs/practice/QIIME/taxa-barplot.qzv
+--i-table /path/to/your/directory/feature-frequency-filtered-table.qza \
+--i-taxonomy /path/to/your/directory/updated_taxonomy.qza \
+--m-metadata-file /path/to/your/directory/bacteria_manifest.tsv \
+--o-visualization /path/to/your/directory/taxa-barplot.qzv
