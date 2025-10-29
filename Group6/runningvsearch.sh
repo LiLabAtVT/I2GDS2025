060c0bb33af6a97f437b2c410e2ae9d5d473bbab
mckennav24 <mckennav24@vt.edu>
2025-10-28 22:29:35 -0400

    Create runningvsearch.sh

---
+++ Group6/runningvsearch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --account=introtogds
+#SBATCH --partition=normal_q
+#SBATCH -t 7-00:00:00
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=mckennav24@vt.edu
+
+module load Miniconda3
+
+source activate diamond_and_vsearch
+
+cd /projects/intro2gds/I2GDS2025/G6_AMR_ARG/McKenna/vsearchinput
+
+samples=$(ls *_[12]_decontam.fastq.gz | awk -F/ '{gsub(/_[12]_decontam.fastq.gz/, "", $NF); print $NF}' | sort | uniq)
+
+for sample in $samples; do
+
+ vsearch --fastq_mergepairs ${sample}_1_decontam.fastq.gz --reverse ${sample}_2_decontam.fastq.gz --fastaout ${sample}_merged.fastq --fastaout_notmerged_fwd ${sample}_unmerged_forward.fastq --fastaout_notmerged_rev ${sample}_unmerged_reverse.fastq --threads 128
+    cat ${sample}_merged.fastq ${sample}_unmerged_forward.fastq ${sample}_unmerged_reverse.fastq > ${sample}_clean_merged.fastq
+    rm ${sample}_merged.fastq ${sample}_unmerged_forward.fastq ${sample}_unmerged_reverse.fastq
+
+done
