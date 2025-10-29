00b59eedf7af31e5220b25609628e03803bd53cd
peterfs-vt <peterfs@vt.edu>
2025-10-29 15:35:54 -0400

    Update data_import.sh

--- Group1/Needed Materials/Scripts/data_import.sh
+++ Group1/Needed Materials/Scripts/data_import.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=normal_q
 #SBATCH --time=0-08:00:00
 #SBATCH --mem=2G
-#SBATCH --mail-user=peterfs@vt.edu
+#SBATCH --mail-user=yourusername@vt.edu
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
@@ -13,10 +13,10 @@ conda activate qiime2-amplicon-2025.7
 
 qiime tools import \
 --type 'SampleData[PairedEndSequencesWithQuality]' \
---input-path /home/peterfs/practice/files/bacteria_manifest.tsv \
---output-path /home/peterfs/practice/QIIME/data.qza \
+--input-path /path/to/your/directory/bacteria_manifest.tsv \
+--output-path /path/to/your/directory/data.qza \
 --input-format PairedEndFastqManifestPhred33V2 \
 
 qiime demux summarize \
---i-data /home/peterfs/practice/QIIME/data.qza \
---o-visualization /home/peterfs/practice/QIIME/qualityplot.qzv\
+--i-data /path/to/your/directory/data.qza \
+--o-visualization /path/to/your/directory/qualityplot.qzv\
